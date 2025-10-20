a948a1ce90b61efe7022105ebbc0975989379808
L30M453V5K1 <leo.masevski@student.um.si>
2025-10-20 21:18:54 +0200

    tests fixed

--- backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
+++ backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.QuestApp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ImportAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 class TaskAppApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
+
