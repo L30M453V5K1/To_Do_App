6f9f3abec5142713c54a58ae3df7434d75a7f7b6
L30M453V5K1 <leo.masevski@student.um.si>
2025-10-20 21:36:06 +0200

    test configurations fix

--- backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
+++ backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.QuestApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskAppApplicationTests {
 
 	@Test
